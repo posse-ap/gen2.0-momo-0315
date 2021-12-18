406738aa8f82a804ca959f5b567c6dac28606cef
momo-0315 <momo1777@asagi.waseda.jp>
2021-12-18 15:13:10 +0900

    とりあえずシャッフルできた

--- Docker.quizy/mysql/sql/init.sql
+++ Docker.quizy/mysql/sql/init.sql
@@ -9,36 +9,28 @@ name TEXT NOT NULL
 )DEFAULT CHARACTER SET=utf8;
  
 INSERT INTO big_questions (name) VALUES ("東京の難読地名クイズ"),("広島の難読地名クイズ");
-
-
-DROP TABLE IF EXISTS questions;
  
-CREATE TABLE questions (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-big_quistion_id INT NOT NULL,
-image TEXT NOT NULL
-)DEFAULT CHARACTER SET=utf8;
- 
-INSERT INTO questions (big_quistion_id,image) VALUES 
-(1,"https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png"),(1,'kameido.png'),(2,'mukainada.png');
-
 
 DROP TABLE IF EXISTS choices;
- 
 CREATE TABLE choices (
-id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-quistion_id INT NOT NULL,
-name TEXT NOT NULL,
-valid INT NOT NULL
-)DEFAULT CHARACTER SET=utf8;
- 
-INSERT INTO choices (quistion_id,name,valid) VALUES 
-(1,"たかなわ",1),
-(1,"こうわ",0),
-(1,"たかわ",0),
-(2,"かめと",0),
-(2,"かめど",0),
-(2,"かめイド",1),
-(3,"むこうひら",0),
-(3,"むきひら",0),
-(3,"むかいなだ",1);
+id INT AUTO_INCREMENT NOT NULL PRIMARY KEY,
+img VARCHAR(225) NOT NULL,
+big_question_id INT NOT NULL,
+question_id INT NOT NULL,
+option_1 VARCHAR(225) NOT NULL,
+option_2 VARCHAR(225) NOT NULL,
+option_3 VARCHAR(225) NOT NULL
+);
+
+INSERT INTO choices (img, big_question_id, question_id, option_1, option_2, option_3) VALUES 
+('https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png',1, 1, 'たかなわ', 'たかわ', 'こうわ'),
+('https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png', 1, 2, 'かめイド', 'かめと', 'かめど'),
+('https://d1khcm40x1j0f.cloudfront.net/quiz/ad4f8badd896f1a9b527c530ebf8ac7f.png', 1, 3, 'こうじまち', 'かゆまち', 'おかとまち'),
+('https://d1khcm40x1j0f.cloudfront.net/quiz/ee645c9f43be1ab3992d121ee9e780fb.png', 1, 4, 'おなりもん', 'おかどもん', 'ごせいもん'),
+('https://d1khcm40x1j0f.cloudfront.net/quiz/6a235aaa10f0bd3ca57871f76907797b.png', 1, 5, 'とどろき', 'たたらき', 'たたら'),
+('https://d1khcm40x1j0f.cloudfront.net/quiz/0b6789cf496fb75191edf1e3a6e05039.png', 1, 6, 'しゃくじい', 'せきこうい', 'いじい'),
+('https://d1khcm40x1j0f.cloudfront.net/quiz/23e698eec548ff20a4f7969ca8823c53.png', 1, 7, 'ぞうしき', 'ざっしき', 'ざっしょく'), 
+('https://d1khcm40x1j0f.cloudfront.net/quiz/50a753d151d35f8602d2c3e2790ea6e4.png', 1, 8, 'おかちまち', 'みとちょ', 'ごしろちょう'),
+('https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png', 1, 9, 'ししぼね', 'ろっこつ', 'しこね'),
+('https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png', 1, 10, 'こぐれ', 'こばく', 'こしゃく'),
+('mukainada.png', 2, 1, 'むかいなだ', 'むおうひら', 'むきひら');
--- Docker.quizy/www/html/quizy.js
+++ Docker.quizy/www/html/quizy.js
@@ -1,27 +1,4 @@
 'use strict';
-// 選択肢の配列
-// let option = [["たかなわ", "こうわ", "たかわ"],//問１の選択肢
-// ["かめいど", "かめと", "かめど"],//問２の選択肢
-// ["こうじまち", "おかとまち", "かゆまち"],//問３の選択肢
-// ["おなりもん", "ごせいもん", "おかどもん"],//問４の選択肢
-// ["とどろき", "たたりき", "たたら"],//問５の選択肢
-// ["しゃくじい", "いじい", "せきこうい"],//問６の選択肢
-// ["ぞうしき", "ざっし", "ざっしき"],//問７の選択肢
-// ["おかちまち", "ごしろまち", "みとちょう"],//問８の選択肢
-// ["ししぼね", "しこね", "ろっこつ"],//問９の選択肢
-// ["こぐれ", "こしゃく", "こばく"]];//問１０の選択肢
-
-// 画像の配列
-// let img = ["https://d1khcm40x1j0f.cloudfront.net/quiz/34d20397a2a506fe2c1ee636dc011a07.png",//画像１
-//     "https://d1khcm40x1j0f.cloudfront.net/quiz/512b8146e7661821c45dbb8fefedf731.png",//画像２
-//     "https://d1khcm40x1j0f.cloudfront.net/quiz/ad4f8badd896f1a9b527c530ebf8ac7f.png",//画像３
-//     "https://d1khcm40x1j0f.cloudfront.net/quiz/ee645c9f43be1ab3992d121ee9e780fb.png",//画像４
-//     "https://d1khcm40x1j0f.cloudfront.net/quiz/6a235aaa10f0bd3ca57871f76907797b.png",//画像５
-//     "https://d1khcm40x1j0f.cloudfront.net/quiz/0b6789cf496fb75191edf1e3a6e05039.png",//画像６
-//     "https://d1khcm40x1j0f.cloudfront.net/quiz/23e698eec548ff20a4f7969ca8823c53.png",//画像７
-//     "https://d1khcm40x1j0f.cloudfront.net/quiz/50a753d151d35f8602d2c3e2790ea6e4.png",//画像８
-//     "https://d1khcm40x1j0f.cloudfront.net/words/8cad76c39c43e2b651041c6d812ea26e.png",//画像９
-//     "https://d1khcm40x1j0f.cloudfront.net/words/34508ddb0789ee73471b9f17977e7c9c.png"];//画像１０
 
 // この地名はなんて読む＋画像＋選択肢+解答ボックスのるーぷ
 for (let i = 0; i < 10; i++) {
@@ -46,22 +23,22 @@ for (let i = 0; i < 10; i++) {
     // htmlに作ったdivタグに入れる
 
     //htmlに作ったdivタグに入れる
-    let listItems = document.querySelectorAll(`#answerLists_${i} li`)
+    // let listItems = document.querySelectorAll(`#answerLists_${i} li`)
 
     //ランダム関数 0,1,2の乱数を取ってくる    
-    function rand() {
-        var choice_order = Math.floor(Math.random() * 3);
-        return choice_order;
-    }
+    // function rand() {
+    //     var choice_order = Math.floor(Math.random() * 3);
+    //     return choice_order;
+    // }
     //htmlコレクションを配列にする styleをorderにする
-    Array.from(listItems).forEach(elm => { elm.style.order = rand() });
+    // Array.from(listItems).forEach(elm => { elm.style.order = rand() });
 };
 
 // 選択肢に引数を与える optionNumberとanswerNumberが同じだったら正解
 var check = function (questionNumber, optionNumber, answerNumber) {
 
     let questionOptionNumber = document.getElementById("answerList_" + questionNumber + "_" + optionNumber);//クリックしたliのidを取得
-    let alwaysAnswerNumber = document.getElementById("answerList_" + questionNumber + "_0");//常に正解（最初の選択肢）を取得
+    let alwaysAnswerNumber = document.getElementById("answerList_" + questionNumber + "_1");//常に正解（最初の選択肢）を取得
     let click_invalidation = document.getElementById('answerLists_' + questionNumber);//三つの選択肢を取得・クリック不可にするため
 
     if (optionNumber === answerNumber) {
